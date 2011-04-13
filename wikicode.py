a64f921af5d67cf7e1017f6bdd54c554ba453ea3
Eleyine Zarour <eleyine@gmail.com>
2011-04-13 16:51:46 -0400

    major improvements

--- wikicode.py
+++ wikicode.py
@@ -5,6 +5,12 @@ import string
 import re
 
 print 'This python script transforms html-formatted google docs to standard wikicode'
+def getAttribute(parent_match):
+  str = parent_match.group()
+  attr_regex = re.compile('</{0,1}[a-zA-Z0-9]*[> ]')
+  attr = attr_regex.search(str).group()
+  attr = string.strip(attr,'</ >')
+  return attr
 
 def getClass(line, match_object):
   fulltag = line[match_object.start():match_object.end()]
@@ -13,48 +19,50 @@ def getClass(line, match_object):
   return attr_match.group(1)
 
 def wikicode(line, stack, start, end):
-  cur_match = stack[-1] # cur is the last opening tag
-  cur = cur_match.group()
+  parent_match = stack[-1] # currently the last opening tag
+  parent = getAttribute(parent_match)
   wikiline = ''  
   
   wikidict = {
-             '<title>':'=',
-             '<ol>':'*',
-             '<h4>':'====',
-             '<h3>':'===',
-             '<h2>':'==',
-             '<h1>':'=',
-             # pretty short I know..
+             'title':['=', '='],
+             'ol':['*', ''],
+             'h4':['====', '===='],
+             'h3':['===', '==='],
+             'h2':['==', '=='],
+             'h1':['=','='],
+             'p': ['','']
+            # pretty short I know..
              }
   oldict = {
            'c2':1,
+           'c9':1,
+           'c8':2,
+           'c10':3,
            'c4':2,
            'c20':3,  
            }
 
   i = -1;
-  parent = cur # Parent is the html tag that contains one of the tags in wikicode
-  parent_match = cur_match
 
-  while parent not in wikidict:
+  while parent not in wikidict.keys():
     i = i - 1;
     if -i >= len(stack):
       break
     parent_match = stack[i]
-    parent = parent_match.group()
+    parent = getAttribute(parent_match)
   
-  format = ""
-  if parent in wikidict:  
-    if parent == '<ol>':
+  left = ""
+  right = ""
+  if parent in wikidict:
+    format = wikidict[parent]
+    if parent == 'ol':
        degree = oldict[getClass(line, parent_match)]
-       format = wikidict[parent] * degree
+       left = format[0] * degree
     else:
-       format = wikidict[parent]
+       left = format[0]
+       right = format[1]
        
-  wikiline = format
-  wikiline = wikiline + line[start:end]
-  wikiline = wikiline + format
-  return wikiline
+  return (left + line[start:end] + right)
 
 def isClosingTag(tag):
   ct = re.compile('^</')
@@ -65,27 +73,28 @@ def isClosingTag(tag):
 
 def getOutput(line):
   # Define tags
-  tagRegex = re.compile('<[a-zA-Z"=/]*>')
+  tagRegex = re.compile('<.*?>') # ('<[a-zA-Z"=/ ]*>')
   iterator = tagRegex.finditer(line)
   stack = [] # stack of match objects
   output = [] # list of wikicode lines
   content = True # The content must be between an opening and a closing tag. This boolean keeps track of this.
 
+  prev = "" # Keeps track of the previous tag
   for match in iterator:
-     tag = match.group()
-     
-     if isClosingTag(tag) == False:
-       print '%(tag)s is an opening tag' %vars()
+     cur = getAttribute(match)
+ 
+     if isClosingTag(match.group()) == False:
        stack.append(match)
        start = match.end()  # The start of the content is the end of the opening tag
-       content = True
+       prev = getAttribute(match)
      else:
-       if content == True:
-         end = match.start()  #The end of the content is the beginning of the opening tag 
+       end = match.start()  #The end of the content is the beginning of the closing tag 
+       if prev == cur:
+         print line[start:end]
          output.append(wikicode(line, stack, start, end))
-       if len(stack) > 0:
-         stack.pop()
-       content = False 
+       else:
+         print '%(prev)s and %(cur)s do not match' %vars()
+       stack.pop()
   return output
 
 # Get .html file
@@ -93,7 +102,7 @@ def getOutput(line):
 #txt = raw_input('Enter output file:')
 #print 'Starting parsing of %(html)s\n' % vars()
 
-html = 'BIOL201LN21.html'
+html = 'SimpleDoc.html'
 file = open(html, 'rt')
 line = file.readline() # LOL Google Docs output html as a single line
 file.close()
